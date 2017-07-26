8ffaff1a3d87d7c7c101462daed7f4eca22a012f
eshell <eshell@example.com>
2017-07-26 13:22:30 +0900

    clear 2

--- o2/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function(req, res){
-        res.send('Hello eshell!');
-});
-app.listen(3001, function(){
-        console.log('Connect 3001 port');
-});
--- server_mum/test.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-app.get('/', function(req, res){
-        res.send('Hello eshell?');
-});
-app.listen(8000, function(){
-        console.log('Connect 8000 port');
-});
